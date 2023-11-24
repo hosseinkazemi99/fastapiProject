9ba9787441ca8a133d9d002d4474df6d98f225d8
hosseinTabaie <hosseinkazemi7896@gmail.com>
2023-11-25 01:37:01 +0330

    create instagram schema and route for login

--- routers/instalogin.py
+++ routers/instalogin.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, HTTPException, APIRouter
 from fastapi.security import OAuth2PasswordBearer
-from schemas.instagram import login_to_instagram
+from schemas.instagram import Instagram
 from motor.motor_asyncio import AsyncIOMotorClient
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 from database import get_database
@@ -12,7 +12,7 @@ router = APIRouter()
 @router.post("/insta/login")
 async def login_insta(username: str, password: str, db: AsyncIOMotorClient = Depends(get_database())):
     try:
-        credentials= await login_to_instagram(username, password)
+        credentials= await Instagram.login(username, password)
         result = await db.insert_one(credentials)
         return {"credentials": str(result.inserted_id)}
     except HTTPException as e:
--- schemas/instagram.py
+++ schemas/instagram.py
@@ -1,19 +1,59 @@
+import json
+from datetime import datetime
+from typing import BinaryIO
 import requests
-from fastapi import HTTPException
-
-async def login_to_instagram(username: str, password: str):
-
-    login_url = "https://www.instagram.com/api/v1/web/accounts/login/ajax/"
-
-    data = {
-        "username": username,
-        "password": password,
-    }
-
-    response = requests.post(login_url, data=data)
 
 
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=400,detail=f"Login failed with status code {response.status_code}")
+class Instagram:
+    def __init__(self):
+        self.session = None
+
+    def load(self, session):
+        self.session = session
+
+    async def login(self, username: str, password: str) -> dict:
+        url = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/api/v1/web/accounts/login/ajax/'
+
+        time = int(datetime.now().timestamp())
+
+        response = requests.get(url)
+        csrf = response.cookies.get('csrftoken')
+
+        payload = {
+            'username': username,
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:{password}',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+
+        login_header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)"
+                          " Chrome/77.0.3865.120 Safari/537.36",
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": "https://www.instagram.com/accounts/login/",
+            "x-csrftoken": 'Vouc7J0oJSpBciIJptNQAxRA5dDkKWq2'
+        }
+
+        login_response = requests.post(login_url, data=payload, headers=login_header)
+        json_data = json.loads(login_response.text)
+        # print(json_data)
+        if json_data.get("authenticated"):
+            cookies = login_response.cookies
+            cookie_jar = cookies.get_dict()
+
+            self.session = {
+                "csrftoken": cookie_jar['csrftoken'],
+                "sessionid": cookie_jar['sessionid'],
+                "ds_user_id": cookie_jar['ds_user_id'],
+                "mid": cookie_jar['mid'],
+                "ig_did": cookie_jar['ig_did'],
+                "rur": cookie_jar['rur'],
+
+            }
+
+            return self.session
+
+        raise Exception(login_response.text)
+
+
